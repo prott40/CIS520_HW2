e71339b43469f0f8bd32dea0251a7a727edf8437
prott40 <143848245+prott40@users.noreply.github.com>
2025-02-21 10:04:08 -0600

    Update analysis.c
    addes the required ../ to the file name so we can access it

--- src/analysis.c
+++ src/analysis.c
@@ -1,6 +1,7 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 // Include headers: dyn_array, processing_scheduling
 #include "dyn_array.h"
 #include "processing_scheduling.h"
@@ -18,20 +19,29 @@ int main(int argc, char **argv)
         printf("%s <pcb file> <schedule algorithm> [quantum]\n", argv[0]);
         return EXIT_FAILURE;
     }
+    // added need ../ to access the file wanted and \n at end of string
+    size_t file_name_length = strnlen(argv[1],20) + 3 + 1;
 
+    char * file_name = malloc(file_name_length);
+    if(!file_name){
+        fprintf(stderr, "%s:%d failed file name malloc\n", __FILE__, __LINE__);
+        return EXIT_FAILURE;
+    }
+    
+    snprintf(file_name,file_name_length,"../%s",argv[1]);
     // Allocate memory for FCFS scheduler results
     ScheduleResult_t *FCFS_Result = (ScheduleResult_t *)malloc(sizeof(ScheduleResult_t));
     if (!FCFS_Result) 
     {
-        fprintf(stderr, "Error: Memory allocation failed for FCFS_Result.\n");
+        fprintf(stderr, "%s:%d failed fcfs result malloc fail\n", __FILE__, __LINE__);
         return EXIT_FAILURE;
     }
 
     // Load the process control blocks from the binary file
-    dyn_array_t *binArray = load_process_control_blocks(argv[0]);
+    dyn_array_t *binArray = load_process_control_blocks(file_name);
     if (!binArray) 
     {
-        fprintf(stderr, "Error: Failed to load process control blocks from file %s.\n", argv[1]);
+        fprintf(stderr, "%s:%d failed load procces control block\n", __FILE__, __LINE__);
         free(FCFS_Result);
         return EXIT_FAILURE;
     }
@@ -42,21 +52,21 @@ int main(int argc, char **argv)
     // Check if the scheduling was successful
     if (res) 
     {
-        printf("Scheduling completed successfully.\n");
+        fprintf(stderr, "%s:%d passed sorting array \n", __FILE__, __LINE__);
     } 
     else 
     {
-        fprintf(stderr, "Scheduling failed.\n");
+        fprintf(stderr, "%s:%d failed load procces control block\n", __FILE__, __LINE__);
         dyn_array_destroy(binArray);
         free(FCFS_Result);
         return EXIT_FAILURE;
     }
 
     // Open the README.md file for appending results
-    FILE *pFile = fopen("README.md", "a");
+    FILE *pFile = fopen("../README.md", "a");
     if (!pFile) 
     {
-        fprintf(stderr, "Error: Failed to open README.md for writing.\n");
+        fprintf(stderr, "%s:%d readme failed to open \n", __FILE__, __LINE__);
         dyn_array_destroy(binArray);
         free(FCFS_Result);
         return EXIT_FAILURE;
@@ -72,6 +82,7 @@ int main(int argc, char **argv)
     // Clean up
     dyn_array_destroy(binArray);
     free(FCFS_Result);
+    free(file_name);
 
     return EXIT_SUCCESS;
 }
